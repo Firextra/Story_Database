22487cea8c14d2e8c21c725e87d0d29b5473156e
Firextra <kyle.cope2004@gmail.com>
2025-02-14 17:37:10 +0000

    initial push

--- Story_Database/models.py
+++ Story_Database/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class UniverseDatabase(models.Model):
+    Name=models.CharField(max_length=64)
+    UniverseNumber=models.PositiveIntegerField(unique=True)
+    UniverseDescription=models.TextField()
+
+    def __str__(self):
+        return (f" name:{self.Name}, universe number:{self.UniverseNumber}, universe description:{self.UniverseDescription}")
+
+
+
+class CharacterDatabase(models.Model):
+    Name = models.CharField(max_length=64)
+    Author = models.CharField(max_length=64)
+    CharacterDescription = models.TextField()
+    UniverseNumber = models.PositiveIntegerField()
+
+    def __str__(self):
+        return (f" name:{self.Name}, author:{self.Author}, character description:{self.CharacterDescription}, universe number:{self.UniverseNumber}")
