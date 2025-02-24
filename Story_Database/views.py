5255de46324c7509852a52513cffaa6ac5f51f09
Firextra <kyle.cope2004@gmail.com>
2025-02-24 20:26:29 +0000

    login test

--- Story_Database/views.py
+++ Story_Database/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import UniverseDatabase, CharacterDatabase
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
        universe=UniverseDatabase.objects.all()
        context={'universe':universe}
        return render(request, "index.html", context)
+
+@login_required
+def Profile(request):
+       return render(request, 'ProfilePage.html')
