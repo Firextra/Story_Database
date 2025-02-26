d6a9acb60966591bc8df6e486b630ee8381c20e1
Firextra <kyle.cope2004@gmail.com>
2025-02-26 20:02:17 +0000

    Why can't I find base.html

--- S267058_Story_Database/settings.py
+++ S267058_Story_Database/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'S267058_Story_Database.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
