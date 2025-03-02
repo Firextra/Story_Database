dbe20b1b6306a3ec6dd3676ce58c687f3a5eb287
Firextra <kyle.cope2004@gmail.com>
2025-03-02 00:32:22 +0000

    base.html fix 7

--- S267058_Story_Database/settings.py
+++ S267058_Story_Database/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'S267058_Story_Database.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "Story_Database" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
